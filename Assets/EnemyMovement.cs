8daa92c18b4bf71b644ab2d9ea8360851a07803b
Rey.Perez <ichibanrei@gmail.com>
2018-08-10 13:52:23 -0700

    initial commit

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    [SerializeField] List<Waypoint> path;
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(FollowPath());
+	}
+
+    IEnumerator FollowPath()
+    {
+        print("Starting patrol...");
+        foreach (Waypoint waypoint in path)
+        {
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
+            print("visiting block: " + waypoint);
+        }
+        print("Ending patrol");
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+	}
+}
